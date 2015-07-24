fbba89028f499d55e06278d6ae144e7bc4b34b40
paul <paul.mckenzie@gmail.com>
2015-07-24 18:13:17 +0100

    Add some more useful step defs

--- src/test/java/crowdmix/AcceptanceTests.java
+++ src/test/java/crowdmix/AcceptanceTests.java
@@ -1,8 +1,10 @@
 package crowdmix;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(tags = "~@wip")
 public class AcceptanceTests {
 }
